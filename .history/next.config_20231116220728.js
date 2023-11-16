84f8e2eae089d87f04c55cd0e0cade4d9f936ad5
yuu0252 <yutanmaron@gmail.com>
2023-11-16 22:51:17 +0900

    modified

--- .history/next.config_20231116220728.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: ['images.microcms-assets.io'],
-  },
-};
-
-module.exports = nextConfig;
--- .history/src/app/contents/layout_20231115090952.tsx
+++
@@ -1,9 +0,0 @@
-import './style.scss';
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <>{children};</>;
-}
